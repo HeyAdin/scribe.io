0e71bd5cf74f22c53cfd802e8cdb32605a2f12c2
heyadin <adinahmad003@gmail.com>
2025-09-19 21:07:27 +0530

    added routing for all the buttons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,14 +6,17 @@ export const Navbar = () => {
     return <div className="w-[80%] flex justify-between items-center fixed z-10">
         <img src={logo} width={"100"} height={"100"} className=" h-auto" />
         <div className='flex list-none gap-5 text-lg text-neutral-700 font-semibold'>
+            <li className='cursor-pointer hover:text-neutral-500' onClick={()=>{navigate("/profile/publish")}}>Create</li>
             <li className='cursor-pointer hover:text-neutral-500' onClick={()=>{navigate("/blogs")}}>Home</li>
             <li className='cursor-pointer hover:text-neutral-500' onClick={()=>{navigate("/about")}}>About</li>
             <li className='group relative cursor-pointer'>
-                <div className='hover:border-b hover:text-neutral-500' onClick={() => { navigate("/blogs") }} >Profile</div>
+                <div className='hover:border-b hover:text-neutral-500' onClick={() => { navigate("/profile") }} >Profile</div>
                 <motion.div
                     className='group-hover:flex flex-col hidden absolute w-[70px] '>
                     <div className='hover:border-b  hover:text-neutral-500' onClick={() => { navigate("/blogs") }}>Settings</div>
-                    <div className='hover:border-b  hover:text-neutral-500' onClick={() => { navigate("/sign-in") }}>Sign out</div>
+                    <div className='hover:border-b  hover:text-neutral-500' onClick={() => { 
+                        localStorage.removeItem("token");
+                        navigate("/sign-in") }}>Sign out</div>
                 </motion.div>
             </li>
         </div>
