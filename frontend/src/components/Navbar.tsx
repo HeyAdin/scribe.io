e105944e998936a04443d9db6ea3d924724f74e2
heyadin <adinahmad003@gmail.com>
2025-09-20 01:52:53 +0530

    routing

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,11 +4,10 @@ import { motion } from "motion/react"
 export const Navbar = () => {
     const navigate = useNavigate();
     return <div className="w-[80%] flex justify-between items-center fixed z-10">
-        <img src={logo} width={"100"} height={"100"} className=" h-auto" />
+        <img onClick={()=>navigate('/blogs')} src={logo} width={"100"} height={"100"} className=" h-auto cursor-pointer" />
         <div className='flex list-none gap-5 text-lg text-neutral-700 font-semibold'>
             <li className='cursor-pointer hover:text-neutral-500' onClick={()=>{navigate("/profile/publish")}}>Create</li>
             <li className='cursor-pointer hover:text-neutral-500' onClick={()=>{navigate("/blogs")}}>Home</li>
-            <li className='cursor-pointer hover:text-neutral-500' onClick={()=>{navigate("/about")}}>About</li>
             <li className='group relative cursor-pointer'>
                 <div className='hover:border-b hover:text-neutral-500' onClick={() => { navigate("/profile") }} >Profile</div>
                 <motion.div
