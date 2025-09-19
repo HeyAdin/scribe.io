2d4f4a76cd3ecf22865e8f37c1f7074b6e1b0eb1
heyadin <adinahmad003@gmail.com>
2025-09-19 20:57:32 +0530

    added routing for profile and viewblog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,28 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
-import { Blog } from './pages/Blog'
+import { Blogs } from './pages/Blogs'
+import { ViewBlogs } from './pages/ViewBlogs'
+import { Profile } from './pages/Profile'
+import { Publish } from './pages/Publish'
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route element={<Signup />} path='sign-up'/>
-      <Route element={<Signin />} path='sign-in' />
-      <Route element ={<Blog />} path='blogs' />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Signup />} path='sign-up' />
+          <Route element={<Signin />} path='sign-in' />
+          <Route path='blogs' >
+            <Route index element={<Blogs />} />
+            <Route path=':id' element={<ViewBlogs />} />
+          </Route>
+          <Route path='profile'>
+            <Route index element={<Profile />} />
+            <Route path='publish' element={<Publish />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
