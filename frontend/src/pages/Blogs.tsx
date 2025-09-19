92fc794091047d3ce771493de01b412720045a47
heyadin <adinahmad003@gmail.com>
2025-09-19 21:05:55 +0530

    added page to display all the blogs

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "../components/Navbar";
+import { BlogContent } from "../components/BlogContent";
+export const Blogs = () => {
+    
+    return <div className="background h-screen flex flex-col items-center ">
+        <Navbar />
+        <BlogContent />
+    </div>
+}
